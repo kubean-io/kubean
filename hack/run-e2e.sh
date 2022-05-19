c84dedb4d738bf41943bebcafdb194b28f035237
yunshihang <shihang.yun@daocloud.io>
2022-05-19 10:48:10 +0800

    update CICD

---
+++ hack/run-e2e.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# This script runs e2e test against on kubean control plane.
+# You should prepare your environment in advance and following environment may be you need to set or use default one.
+# - CONTROL_PLANE_KUBECONFIG: absolute path of control plane KUBECONFIG file.
+#
+# Usage: hack/run-e2e.sh
+KUBECONFIG_PATH=${KUBECONFIG_PATH:-"${HOME}/.kube"}
+
+HOST_CLUSTER_NAME=${1:-"kubean-host"}
+MAIN_KUBECONFIG=${MAIN_KUBECONFIG:-"${KUBECONFIG_PATH}/${HOST_CLUSTER_NAME}.config"}
+MEMBER_CLUSTER_1_NAME=${2:-"member1"}
+MEMBER_CLUSTER_1_KUBECONFIG=${MEMBER_CLUSTER_1_KUBECONFIG:-"${KUBECONFIG_PATH}/${MEMBER_CLUSTER_1_NAME}.config"}
+MEMBER_CLUSTER_2_NAME=${3:-"member2"}
+MEMBER_CLUSTER_2_KUBECONFIG=${MEMBER_CLUSTER_2_KUBECONFIG:-"${KUBECONFIG_PATH}/${MEMBER_CLUSTER_2_NAME}.config"}
+
+# Install ginkgo
+GOPATH=$(go env GOPATH | awk -F ':' '{print $1}')
+export PATH=$PATH:$GOPATH/bin
+
+# Run e2e
+# kubean-apiserver's svc is kubean
+kubectl --kubeconfig="${MAIN_KUBECONFIG}" patch svc kubean -n kubean-system -p '{"spec": {"type": "NodePort"}}'
+
+# Note: After ginkgo 2.1.4, to pass additional arguments or custom flags down to your suite
+# no need to use -- to separate your arguments from arguments intended for ginkgo
+ginkgo run -v -race --fail-fast ./test/e2e/ --kubeconfig="${MAIN_KUBECONFIG}" --member1="${MEMBER_CLUSTER_1_KUBECONFIG}" --member2="${MEMBER_CLUSTER_2_KUBECONFIG}" --controlplane="${HOST_CLUSTER_NAME}"-control-plane
