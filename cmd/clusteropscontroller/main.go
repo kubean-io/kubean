138528d5b4cd14ba336f13851fe3fef421a3137d
nathan <hangscer@hotmail.com>
2022-05-20 14:00:40 +0800

    add controller

--- cmd/clusteropscontroller/main.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main() {
-	// 监听KuBeanClusterOps的创建 执行启动相关Job等功能
-}
--- cmd/kubeanjob/main.go
+++ cmd/kubeanjob/main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
+	// 不需要
 	// FROM kubespray:2.18
 	// 是Job的执行主体内容 打包镜像 执行 preHook 执行ansible-playbook postHook 等流程
 }
