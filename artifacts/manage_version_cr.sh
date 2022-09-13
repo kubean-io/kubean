5829c86723135001c569c788955bfbfd493ad3b0
bo.jiang <bo.jiang@daocloud.io>
2022-09-13 12:54:08 +0800

    Update helm charts CI

--- artifacts/manage_version_cr.sh
+++ artifacts/manage_version_cr.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-OPTION=${1:-'create_offlineversion'} ## create_offlineversion  create_componentsversion
+OPTION=${1:-'create_offlineversion'} ## create_offlineversion  create_infomanifest
 
 KUBESPRAY_TAG=${KUBESPRAY_TAG:-"v2.19.0"} ## env from github action
 KUBEAN_TAG=${KUBEAN_TAG:-"v0.1.0"}        ## env from github action
@@ -12,12 +12,12 @@ CURRENT_DATE=$(date +%Y%m%d)
 
 ARTIFACTS_TEMPLATE_DIR=artifacts/template
 KUBEAN_OFFLINE_VERSION_TEMPLATE=${ARTIFACTS_TEMPLATE_DIR}/kubeanofflineversion.template.yml
-KUBEAN_COMPONENTS_VERSION_TEMPLATE=${ARTIFACTS_TEMPLATE_DIR}/kubeanclusterconfig.template.yml
+KUBEAN_INFO_MANIFEST_TEMPLATE=${ARTIFACTS_TEMPLATE_DIR}/kubeaninfomanifest.template.yml
 
 CHARTS_TEMPLATE_DIR=charts/kubean/templates
 OFFLINE_PACKAGE_DIR=${KUBEAN_TAG}
 KUBEAN_OFFLINE_VERSION_CR=${OFFLINE_PACKAGE_DIR}/kubeanofflineversion.cr.yaml
-KUBEAN_COMPONENTS_VERSION_CR=${CHARTS_TEMPLATE_DIR}/kubeanclusterconfig.cr.yaml
+KUBEAN_INFO_MANIFEST_CR=${CHARTS_TEMPLATE_DIR}/kubeaninfomanifest.cr.yaml
 
 KUBESPRAY_DIR=kubespray
 KUBESPRAY_OFFLINE_DIR=${KUBESPRAY_DIR}/contrib/offline
@@ -107,18 +107,18 @@ function create_offline_version_cr() {
   update_docker_offline_version "redhat-7" "${docker_version_range_redhat7}"
 }
 
-function update_components_version_cr() {
+function update_info_manifest_cr() {
   index=$1 ## start with zero
   name=$2  ## cni containerd ...
   default_version_val=$3
   version_range=$4
-  if [ $(yq ".spec.components[$index].name" $KUBEAN_COMPONENTS_VERSION_CR) != "${name}" ]; then
+  if [ $(yq ".spec.components[$index].name" $KUBEAN_INFO_MANIFEST_CR) != "${name}" ]; then
     echo "error param $index $name"
     exit 1
   fi
 
-  yq -i ".spec.components[$index].defaultVersion=\"${default_version_val}\"" $KUBEAN_COMPONENTS_VERSION_CR
-  yq -i ".spec.components[$index].versionRange |=  ${version_range}" $KUBEAN_COMPONENTS_VERSION_CR ## update string array
+  yq -i ".spec.components[$index].defaultVersion=\"${default_version_val}\"" $KUBEAN_INFO_MANIFEST_CR
+  yq -i ".spec.components[$index].versionRange |=  ${version_range}" $KUBEAN_INFO_MANIFEST_CR ## update string array
 }
 
 function update_docker_component_version() {
@@ -127,13 +127,18 @@ function update_docker_component_version() {
   version_range=$3
 
   OS=$os yq -i ".spec.docker |= map(select(.os == strenv(OS)).defaultVersion=\"${default_version}\")" \
-    $KUBEAN_COMPONENTS_VERSION_CR
+    $KUBEAN_INFO_MANIFEST_CR
 
   OS=$os yq -i ".spec.docker |= map(select(.os == strenv(OS)).versionRange |= ${version_range})" \
-    $KUBEAN_COMPONENTS_VERSION_CR
+    $KUBEAN_INFO_MANIFEST_CR
 }
 
-function create_components_version_cr() {
+function update_info_manifest_cr_name() {
+  kubean_version=${KUBEAN_TAG//./_}
+  yq -i ".metadata.name=\"kubeaninfomanifest_${kubean_version}\"" $KUBEAN_INFO_MANIFEST_CR
+}
+
+function create_info_manifest_cr() {
   cni_version_default=$(extract_version "cni_version")
   cni_version_range=$(extract_version_range ".cni_binary_checksums.amd64")
 
@@ -157,16 +162,17 @@ function create_components_version_cr() {
   docker_version_range_debian=$(extract_docker_version_range "debian")
   docker_version_range_ubuntu=$(extract_docker_version_range "ubuntu")
 
-  cp $KUBEAN_COMPONENTS_VERSION_TEMPLATE $KUBEAN_COMPONENTS_VERSION_CR
-  KUBESPRAY_TAG=${KUBESPRAY_TAG} yq -i '.spec.kubesprayVersion=strenv(KUBESPRAY_TAG)' $KUBEAN_COMPONENTS_VERSION_CR
-  KUBEAN_TAG=${KUBEAN_TAG} yq -i '.spec.kubeanVersion=strenv(KUBEAN_TAG)' $KUBEAN_COMPONENTS_VERSION_CR
+  cp $KUBEAN_INFO_MANIFEST_TEMPLATE $KUBEAN_INFO_MANIFEST_CR
+  KUBESPRAY_TAG=${KUBESPRAY_TAG} yq -i '.spec.kubesprayVersion=strenv(KUBESPRAY_TAG)' $KUBEAN_INFO_MANIFEST_CR
+  KUBEAN_TAG=${KUBEAN_TAG} yq -i '.spec.kubeanVersion=strenv(KUBEAN_TAG)' $KUBEAN_INFO_MANIFEST_CR
 
-  update_components_version_cr 0 cni "${cni_version_default}" "${cni_version_range}"
-  update_components_version_cr 1 containerd "${containerd_version_default}" "${containerd_version_range}"
-  update_components_version_cr 2 kube "${kube_version_default}" "${kube_version_range}"
-  update_components_version_cr 3 calico "${calico_version_default}" "${calico_version_range}"
-  update_components_version_cr 4 cilium "${cilium_version_default}" "${cilium_version_range}"
-  update_components_version_cr 5 etcd "${etcd_version_default}" "${etcd_version_range}"
+  update_info_manifest_cr_name
+  update_info_manifest_cr 0 cni "${cni_version_default}" "${cni_version_range}"
+  update_info_manifest_cr 1 containerd "${containerd_version_default}" "${containerd_version_range}"
+  update_info_manifest_cr 2 kube "${kube_version_default}" "${kube_version_range}"
+  update_info_manifest_cr 3 calico "${calico_version_default}" "${calico_version_range}"
+  update_info_manifest_cr 4 cilium "${cilium_version_default}" "${cilium_version_range}"
+  update_info_manifest_cr 5 etcd "${etcd_version_default}" "${etcd_version_range}"
   update_docker_component_version "redhat-7" "${docker_version_default}" "${docker_version_range_redhat7}"
   update_docker_component_version "debian" "${docker_version_default}" "${docker_version_range_debian}"
   update_docker_component_version "ubuntu" "${docker_version_default}" "${docker_version_range_ubuntu}"
@@ -178,9 +184,9 @@ create_offlineversion)
   create_offline_version_cr
   ;;
 
-create_componentsversion|create_clusterconfig)
+create_infomanifest)
   check_dependencies
-  create_components_version_cr
+  create_info_manifest_cr
   ;;
 
 *)
