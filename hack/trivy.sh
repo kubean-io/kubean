b9cef701baea8e393debc72796177a5859ae103a
yanggang <gang.yang@daocloud.io>
2022-10-19 16:52:05 +0800

    trivy : add arch environment download and install

--- hack/trivy.sh
+++ hack/trivy.sh
@@ -6,11 +6,20 @@ set -o pipefail
 # checkout command exist
 trivy -v
 if [[ $? != 0 ]];then
-  wget https://github.com/aquasecurity/trivy/releases/download/v0.29.0/trivy_0.29.0_Linux-64bit.tar.gz
-  tar xf trivy_0.29.0_Linux-64bit.tar.gz
-  mv trivy /usr/local/bin/trivy
+  arch=$(arch)
+  case $(arch) in
+  "x86_64")
+    wget https://github.com/aquasecurity/trivy/releases/download/v0.29.0/trivy_0.29.0_Linux-64bit.tar.gz
+    tar xf trivy_0.29.0_Linux-64bit.tar.gz
+    ;;
+  "aarch64")
+    wget https://github.com/aquasecurity/trivy/releases/download/v0.29.0/trivy_0.29.0_Linux-ARM64.tar.gz
+    tar xf trivy_0.29.0_Linux-ARM64.tar.gz
+    ;;
+  esac
 fi
-
+mv trivy /usr/local/bin/trivy
+trivy -v
 for i in $*
 do
   trivy image --ignore-unfixed --exit-code 0 --severity MEDIUM,HIGH,CRITICAL ${i}
