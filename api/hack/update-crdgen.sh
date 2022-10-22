289e3ca4917ee81cd313c9003cc3b58bea1cf051
yanggang <gang.yang@daocloud.io>
2022-10-22 13:39:42 +0800

    delete duplicate code for hack/util.sh

--- api/hack/update-crdgen.sh
+++ api/hack/update-crdgen.sh
@@ -7,7 +7,8 @@ set -o pipefail
 CONTROLLER_GEN_PKG="sigs.k8s.io/controller-tools/cmd/controller-gen"
 CONTROLLER_GEN_VER="v0.6.2"
 
-source hack/util.sh
+REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/../..
+source "${REPO_ROOT}"/hack/util.sh
 
 echo "Generating with controller-gen"
 util::install_tools ${CONTROLLER_GEN_PKG} ${CONTROLLER_GEN_VER} >/dev/null 2>&1
