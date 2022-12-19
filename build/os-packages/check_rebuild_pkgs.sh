c49d65bd0ba17274e82030b45ff168b4889abf23
tu1h <lihai.tu@daocloud.io>
2022-12-19 15:03:42 +0800

    Remove unnecessary pkgs
    Signed-off-by: tu1h <lihai.tu@daocloud.io>

--- build/os-packages/check_rebuild_pkgs.sh
+++ build/os-packages/check_rebuild_pkgs.sh
@@ -12,25 +12,25 @@ OS_NAME=${OS_NAME:-""}
 ORG_NAME=${ORG_NAME:-""}
 
 # Get Latest Git Tag
-late_tag=`git tag --sort=committerdate -l | grep -o 'v.*' | tail -1`
+late_tag=$(git tag --sort=committerdate -l | grep -o 'v.*' | tail -1)
 # Get Previous Git Tag (the one before the latest tag)
-prev_tag=`git tag --sort=committerdate -l | grep -o 'v.*' | tail -2 | head -1`
+prev_tag=$(git tag --sort=committerdate -l | grep -o 'v.*' | tail -2 | head -1)
 
-wget -c https://raw.githubusercontent.com/${ORG_NAME}/kubean/${late_tag}/build/os-packages/packages.yml -O late_packages.yml
-wget -c https://raw.githubusercontent.com/${ORG_NAME}/kubean/${prev_tag}/build/os-packages/packages.yml -O prev_packages.yml
+# if whole content unchanged, return at once
+git diff --quiet "${prev_tag}" "${late_tag}" build/os-packages/packages.yml && { echo "false"; exit; }
+
+late_packages_yml=$(git show "${late_tag}":build/os-packages/packages.yml)
+prev_packages_yml=$(git show "${prev_tag}":build/os-packages/packages.yml)
 
 if [ "${OS_NAME}" == "kylinv10" ]; then
-  ret=`git diff ${prev_tag} ${late_tag} build/os-packages/repos/kylin.repo`
-  if [ ! -z "$ret" ]; then
-      echo "true" && exit 0
-  fi
+  git diff --quiet "${prev_tag}" "${late_tag}" build/os-packages/repos/kylin.repo && { echo "true"; exit; }
 fi
 
 # centos7 / kylinv10 / redhat7 / redhat8
 if [ "${OS_NAME}" == "centos7" ] || [ "${OS_NAME}" == "kylinv10" ] || [ "${OS_NAME}" == "redhat7" ] || [ "${OS_NAME}" == "redhat8" ]; then
-  late_digest=`yq eval ".yum[],.common[],.docker.${OS_NAME}[]" late_packages.yml | sort | sha1sum | awk '{print $1}'`
-  prev_digest=`yq eval ".yum[],.common[],.docker.${OS_NAME}[]" prev_packages.yml | sort | sha1sum | awk '{print $1}'`
-  if [ ${late_digest} == ${prev_digest} ]; then
+  late_digest=$(echo "${late_packages_yml}" | yq eval ".yum[],.common[],.docker.${OS_NAME}[]" | sort | sha1sum | awk '{print $1}')
+  prev_digest=$(echo "${prev_packages_yml}" | yq eval ".yum[],.common[],.docker.${OS_NAME}[]" | sort | sha1sum | awk '{print $1}')
+  if [ "${late_digest}" == "${prev_digest}" ]; then
     ret=$(wget -c https://github.com/${ORG_NAME}/kubean/releases/download/${prev_tag}/os-pkgs-${OS_NAME}-${prev_tag}.tar.gz -O os-pkgs-${OS_NAME}-${late_tag}.tar.gz)
     if [ $? -eq 0 ]; then
       echo "false" && exit 0
@@ -40,9 +40,9 @@ fi
 
 # ubuntu1804 / ubuntu2004
 if [ "${OS_NAME}" == "ubuntu1804" ] || [ "${OS_NAME}" == "ubuntu2004" ]; then
-  late_digest=`yq eval ".common[],.docker.${OS_NAME}[]" late_packages.yml | sort | sha1sum | awk '{print $1}'`
-  prev_digest=`yq eval ".common[],.docker.${OS_NAME}[]" prev_packages.yml | sort | sha1sum | awk '{print $1}'`
-  if [ ${late_digest} == ${prev_digest} ]; then
+  late_digest=$(echo "${late_packages_yml}" | yq eval ".common[],.docker.${OS_NAME}[]" | sort | sha1sum | awk '{print $1}')
+  prev_digest=$(echo "${prev_packages_yml}" | yq eval ".common[],.docker.${OS_NAME}[]" | sort | sha1sum | awk '{print $1}')
+  if [ "${late_digest}" == "${prev_digest}" ]; then
     ret=$(wget -c https://github.com/${ORG_NAME}/kubean/releases/download/${prev_tag}/os-pkgs-${OS_NAME}-${prev_tag}.tar.gz -O os-pkgs-${OS_NAME}-${late_tag}.tar.gz)
     if [ $? -eq 0 ]; then
       echo "false" && exit 0
