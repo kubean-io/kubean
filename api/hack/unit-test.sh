b0d4f317c6b47b3ec11c6fbae93a35e618dd9724
ErikJiang <ErikJiang@users.noreply.github.com>
2023-05-14 03:43:38 +0000

    deploy: cfb102e82b214fa666369bcf1d1d2f6fae286488

--- api/hack/unit-test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-go test github.com/kubean-io/kubean-api/generated
-go test github.com/kubean-io/kubean-api/apis
-go test github.com/kubean-io/kubean-api/apis/manifest/v1alpha1
