5e55255dd38f62711366b7afa00f62e94db90ea2
tu1h <lihai.tu@daocloud.io>
2023-06-01 10:13:58 +0800

    auto update changes

--- api/generated/localartifactset/localartifactset/clientset/versioned/clientset.go
+++ api/generated/localartifactset/localartifactset/clientset/versioned/clientset.go
@@ -17,8 +17,7 @@ type Interface interface {
 	KubeanV1alpha1() kubeanv1alpha1.KubeanV1alpha1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	kubeanV1alpha1 *kubeanv1alpha1.KubeanV1alpha1Client
