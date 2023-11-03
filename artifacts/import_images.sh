7df5ffd492a3da4f233eb36e852ec9261d498f04
tu1h <lihai.tu@daocloud.io>
2023-11-03 16:36:58 +0800

    Change image artifact to OCI format
    Signed-off-by: tu1h <lihai.tu@daocloud.io>

--- artifacts/import_images.sh
+++ artifacts/import_images.sh
@@ -21,14 +21,14 @@ function check_local_image_files() {
   if [ $(ls -A offline-images | wc -l) -eq 0 ]; then
     return
   fi
-  for dir_name in offline-images/*; do
-    if skopeo inspect dir:"$dir_name" >/dev/null 2>&1; then
-      echo "check $dir_name successfully"
+  while read -r image_name; do
+    if skopeo inspect "oci:offline-images:$image_name" >/dev/null 2>&1; then
+      echo "check $image_name successfully"
     else
-      echo "check $dir_name and $dir_name maybe bad image file"
+      echo "$image_name maybe bad image file"
       exit 1
     fi
-  done
+  done < offline-images/images.list
 }
 
 function import_images() {
@@ -39,7 +39,7 @@ function import_images() {
 
   check_local_image_files
 
-  for dir_name in offline-images/*; do
+  while read -r image_name; do
     copy_cmd="skopeo copy --insecure-policy -a "
     if [ "$DEST_TLS_VERIFY" = false ]; then
       copy_cmd=$copy_cmd" --dest-tls-verify=false "
@@ -47,15 +47,12 @@ function import_images() {
     if [ -n "$DEST_USER" ]; then
       copy_cmd=$copy_cmd" --dest-creds=$DEST_USER:$DEST_PASS "
     fi
-    ## dir:offline-images/abc.com%coreos%etcd:v3.5.4 docker://1.2.3.4:5000/abc.com/coreos/etcd:v3.5.4
-    image_name=${dir_name#*/}      # remove dir prefix
-    image_name=${image_name//%/\/} # replace % with /
-    image_name="$REGISTRY_ADDR"/"$image_name"
+    target_image_name="$REGISTRY_ADDR"/"$image_name"
 
-    echo "import $dir_name to $image_name"
+    echo "import $image_name to $target_image_name"
 
-    $copy_cmd --retry-times=3 dir:"$dir_name" docker://"$image_name"
-  done
+    $copy_cmd --retry-times=3 "oci:offline-images:$image_name" "docker://$target_image_name"
+  done < offline-images/images.list
 
   echo "import completed!"
 }
