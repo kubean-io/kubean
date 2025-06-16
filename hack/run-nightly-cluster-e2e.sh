d5ea61a7f01aa8c2643d4b42d0af61bb45b6e50f
panyintian.fu <1416297674@qq.com>
2025-06-16 13:28:32 +0800

    [Test] Update the K8S version in upgrade test && Cilium pod check
    Signed-off-by: panyintian.fu <1416297674@qq.com>

--- hack/run-nightly-cluster-e2e.sh
+++ hack/run-nightly-cluster-e2e.sh
@@ -146,26 +146,26 @@ func_prepare_config_yaml "${SOURCE_CONFIG_PATH}"  "${dest_config_path}"
 CLUSTER_OPERATION_NAME1="cluster1-install-"`date "+%H-%M-%S"`
 
 # set vars-conf-cm.yml
-sed -i "s/1.31.6/1.31.0/"  "${dest_config_path}"/vars-conf-cm.yml
+sed -i "s/1.31.6/1.31.6/"  "${dest_config_path}"/vars-conf-cm.yml
 
 sed -i "s/e2e-cluster1-install/${CLUSTER_OPERATION_NAME1}/"  "${dest_config_path}"/kubeanClusterOps.yml
 
 
-## prepare cluster upgrade job yml --> upgrade from v1.31.0 to v1.31.2
+## prepare cluster upgrade job yml --> upgrade from v1.31.6 to v1.31.9
 dest_config_path="${REPO_ROOT}"/test/kubean_sonobouy_nightlye2e/e2e-upgrade-cluster-y/
 func_prepare_config_yaml "${SOURCE_CONFIG_PATH}"  "${dest_config_path}"
 CLUSTER_OPERATION_NAME2="cluster1-upgrade-y"
 sed -i "s/e2e-cluster1-install/${CLUSTER_OPERATION_NAME2}/"  "${dest_config_path}"/kubeanClusterOps.yml
 sed -i "s/cluster.yml/upgrade-cluster.yml/" "${dest_config_path}"/kubeanClusterOps.yml
-sed -i "s/1.31.6/1.31.2/"  "${dest_config_path}"/vars-conf-cm.yml
+sed -i "s/1.31.6/1.31.9/"  "${dest_config_path}"/vars-conf-cm.yml
 
-## prepare cluster upgrade job yml --> upgrade from v1.31.2 to v1.31.6
+## prepare cluster upgrade job yml --> upgrade from v1.31.9 to v1.32.1
 dest_config_path="${REPO_ROOT}"/test/kubean_sonobouy_nightlye2e/e2e-upgrade-cluster-z/
 func_prepare_config_yaml "${SOURCE_CONFIG_PATH}"  "${dest_config_path}"
 CLUSTER_OPERATION_NAME3="cluster1-upgrade-z"
 sed -i "s/e2e-cluster1-install/${CLUSTER_OPERATION_NAME3}/"  "${dest_config_path}"/kubeanClusterOps.yml
 sed -i "s/cluster.yml/upgrade-cluster.yml/" "${dest_config_path}"/kubeanClusterOps.yml
-sed -i "s/1.31.6/1.31.6/"  "${dest_config_path}"/vars-conf-cm.yml
+sed -i "s/1.31.6/1.32.1/"  "${dest_config_path}"/vars-conf-cm.yml
 
 ginkgo -v -race -timeout=6h --fail-fast ./test/kubean_sonobouy_nightlye2e/  -- --kubeconfig="${KUBECONFIG_FILE}" \
           --clusterOperationName="${CLUSTER_OPERATION_NAME1}"  --vmipaddr="${vm_ip_addr1}" --vmipaddr2="${vm_ip_addr2}" \
